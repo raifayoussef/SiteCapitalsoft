8dc26089335e4778f1b02b04f2f249a7e011eefe
raifa youssef <yucefr@gmail.com>
2023-07-20 12:03:22 +0100

    push working

---
+++ Capital DISTIBUTION/POINT_VENTE.php
@@ -0,0 +1,440 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home One CapitalSoft</title>
+	<!-- google font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+	<!-- favicons Icons -->
+	<link rel="apple-touch-icon" sizes="180x180" href="../assets/images/favicons/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="../assets/images/favicons/CapitalSoftLogo 32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicons/CapitalSoftLogo 32x32.png" />
+	<link rel="manifest" href="../assets/images/favicons/site.webmanifest" />
+	<!-- plugins css -->
+	<link rel="stylesheet" href="../assets/vendors/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../assets/vendors/fontawesome/css/all.min.css">
+	<link rel="stylesheet" href="../assets/vendors/jarallax/jarallax.css">
+	<link rel="stylesheet" href="../assets/vendors/animate/animate.min.css">
+	<link rel="stylesheet" href="../assets/vendors/owl-carousel/../assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="../assets/vendors/owl-carousel/../assets/owl.theme.default.min.css">
+	<link rel="stylesheet" href="../assets/vendors/cretech-icons/style.css">
+	<!-- templates css -->
+	<link rel="stylesheet" href="../assets/css/cretech.css">
+</head>
+
+<body class="custom-cursor">
+
+<div class="custom-cursor__cursor"></div>
+	<div class="custom-cursor__cursor-two"></div>
+
+	<div class="preloader">
+		<div class="preloader__circle"></div>
+	</div>
+
+	<div class="page-wrapper">
+    <?php
+        require"../inc/topbar.php"; 
+    ;?>
+		<nav class="main-menu sticky-header">
+			<div class="container-fluid">
+				<div class="main-menu__logo">
+					<a href="../index.php">
+						<img src="../assets/images/Logo/CapitalSoftLogo.png" width="98" height="auto" alt="Cretech">
+						<!-- <img src="../assets/images/Logo/CapitalSoftLogo.png" width="98" height="33" alt="Cretech"> -->
+					</a>
+				</div><!-- /.main-menu__logo -->
+
+				<ul class="main-menu__list">
+					<li class="menu-item-has-children">
+						<a href="index.php">Accueil</a>
+					</li>
+					<li class="menu-item-has-children">
+						<a href="about.php">À propos</a>
+					</li>
+					<li class="menu-item-has-children">
+						<a href="services-1.html">Services</a>
+						<ul>
+							<li><a href="projects.html">Études et analyses</a></li>
+							<li><a href="project-details.html">Développement sur mesure</a></li>
+							<li><a href="project-details.html">Intégration de solutions </a></li>
+							<li><a href="project-details.html">Formation et support</a></li>
+						</ul>
+					</li>
+					<li class="menu-item-has-children">
+						<a href="projects.html">Solutions</a>
+						<ul>
+							<li><a href="../Capital SIRH">Capital SIRH</a></li>
+							<li><a href="../Capital FINANCE">Solution 2</a></li>
+							<li><a href="../Capital DISTIBUTION">Solution 3</a></li>
+						</ul>
+					</li>
+					<li class="menu-item-has-children">
+						<a href="blog.html">Carriére</a>
+					</li>
+				</ul><!-- /.main-menu__list -->
+
+				<div class="main-menu__right">
+					<a href="#" class="mobile-nav__toggler">
+						<span></span>
+						<span></span>
+						<span></span>
+					</a>
+					<a href="#" class="search-toggler">
+						<i class="icon-magnifying-glass"></i>
+					</a><!-- /.search-toggler -->
+					<a href="tel:+9288009860" class="main-menu__cta">
+						<i class="fa fa-phone-alt"></i>
+						<span class="main-menu__cta__text">
+							<b>+212 5 22 22 39 99</b>
+							Appelez à tout moment
+						</span><!-- /.main-menu__cta__text -->
+					</a><!-- /.main-menu__cta -->
+				</div><!-- /.main-menu__right -->
+
+			</div><!-- /.container-fluid -->
+		</nav><!-- /.main-menu -->
+
+
+
+		<div class="page-header">
+			<div class="page-header__bg"
+				style="background-image: url(../assets/images/solution/ESPACERH.jpg);"></div>
+			<!-- /.page-header__bg -->
+			<div class="container">
+				<ul class="thm-breadcrumb list-unstyled">
+					<li><a href="../Capital SIRH/index.php">CAPITAL SIRH</a></li>
+					<li>HRT</li>
+				</ul>
+				<h2 class="page-header__title">Espace RH</h2>
+				<p style="font-size: 1.5rem; color: #ffffff;" >Votre espace RH, simplifié et optimisé 
+                pour une gestion efficace du capital humain.</p><!-- /.page-header__title -->
+			</div><!-- /.container -->
+		</div><!-- /.page-header -->
+
+		<section class="section-padding--bottom section-padding--top service-details--page">
+			<div class="container">
+				<div class="row ">
+					<div class="col-lg-8">
+						
+						<h3 class="service-details__title">ESPACE RH</h3><!-- /.service-details__title -->
+						<div class="service-details__content">
+							<p>Capital Espace RH est une solution complète de gestion des workflow 
+								de validation, ainsi que de digitalisation des bulletins de paie et
+ 								des différentes attestations au sein d'une entreprise. Elle offre 
+								une plateforme centralisée pour la gestion efficace de toutes les 
+								demandes liées à la fonction RH, l'automatisation des processus de 
+								validation, et la numérisation des documents de paie et des 
+								attestations courantes.
+                                 </p>					
+						</div><!-- /.service-details__content -->
+						<div class="row" style="margin-top: -64px;">						
+							<div class="col-md-12">
+								<div class="accrodion-grp service-details__accrodion"
+									data-grp-name="service-details__accrodion-1">
+									<h3 class="service-details__caption__title">Fonctionnalités :</h3>
+									<!--Start Faq One Single-->
+									<div class="accrodion wow fadeInUp" data-wow-delay="0ms">
+										<div class="accrodion-title">
+											<h4>Planification et demande de congé en ligne<span
+													class="accrodion-icon"></span>
+											</h4>
+										</div>
+										<div class="accrodion-content">
+											<div class="inner">
+												<p>Les employés peuvent planifier leurs congés annuels, et soumettre 
+leurs demandes de congé via une interface conviviale en ligne, 
+spécifiant les dates, la durée et le type de congé.</p>
+											</div>
+										</div>
+									</div>
+									<!-- End Faq One Single-->
+									<!--Start Faq One Single-->
+									<div class="accrodion  wow fadeInUp" data-wow-delay="0ms">
+										<div class="accrodion-title">
+											<h4>Workflow de validation <span
+													class="accrodion-icon"></span></h4>
+										</div>
+										<div class="accrodion-content">
+											<div class="inner">
+												<p>ESPACE RH gère le workflow de validation des congés en 
+                                                    acheminant les demandes aux responsables appropriés pour 
+                                                    approbation. Les responsables peuvent valider ou rejeter
+                                                    les demandes de congé en ligne, avec des fonctionnalités 
+                                                    de commentaire pour une communication transparente.</p>
+											</div>
+										</div>
+									</div>
+									<!-- End Faq One Single-->
+									<!--Start Faq One Single-->
+									<div class="accrodion  wow fadeInUp" data-wow-delay="0ms">
+										<div class="accrodion-title">
+											<h4>Suivi des soldes de congés <span
+													class="accrodion-icon"></span></h4>
+										</div>
+										<div class="accrodion-content">
+											<div class="inner">
+											<p>La solution ESPACE RH maintient à jour les soldes de congés des 
+                                                employés, en tenant compte des congés accordés, des congés 
+                                                restants et des congés déjà pris. Cela permet aux employés 
+                                                et aux Responsables et Gestionnaires 
+												RH de visualiser rapidement les informations 
+                                                sur les congés de l’ensemble du personnel.</p>
+											</div>
+										</div>
+									</div>
+									<!-- End Faq One Single-->
+									<!--Start Faq One Single-->
+									<div class="accrodion  wow fadeInUp" data-wow-delay="0ms">
+										<div class="accrodion-title">
+											<h4>Digitalisation des bulletins de paie  <span
+													class="accrodion-icon"></span>
+											</h4>
+										</div>
+										<div class="accrodion-content">
+											<div class="inner">
+											<p>La solution Espace RH permet aux employés  d’accéder à leurs 
+												bulletins de paie en ligne à tout moment, éliminant ainsi le besoin 
+												de versions papier.</p>
+											</div>
+										</div>
+									</div>
+									<!-- End Faq One Single-->
+									<!--Start Faq One Single-->
+									<div class="accrodion  wow fadeInUp" data-wow-delay="0ms">
+										<div class="accrodion-title">
+											<h4>Digitalisation des attestations  <span
+													class="accrodion-icon"></span>
+											</h4>
+										</div>
+										<div class="accrodion-content">
+											<div class="inner">
+											<p>La solution Espace RH est dotée d’un workflow digital des différentes 
+												demandes des attestations courantes, telles que les attestations de travail,
+ 												les attestations de salaire, les attestations de domiciliation, etc. </p>
+											</div>
+										</div>
+									</div>
+									<!-- End Faq One Single-->
+									<!--Start Faq One Single-->
+									<div class="accrodion  wow fadeInUp" data-wow-delay="0ms">
+										<div class="accrodion-title">
+											<h4>Notifications et alertes <span
+													class="accrodion-icon"></span>
+											</h4>
+										</div>
+										<div class="accrodion-content">
+											<div class="inner">
+											<p>La solution ESPACE RH est équipée d'une fonctionnalité de 
+                                                notification et d'alerte intégrée qui permet d'informer 
+                                                rapidement les Responsables 
+												et Gestionnaires RH ainsi que les employés des événements importants 
+												liés à la gestion des congés, au workflow de validation et à la
+ 												digitalisation des bulletins de paie et des attestations. 
+												Cette fonctionnalité envoie des notifications automatisées par e-mails,
+ 												assurant une communication efficace et instantanée.
+</p>
+											</div>
+										</div>
+									</div>
+									<!-- End Faq One Single-->
+									
+								</div>
+							</div><!-- /.col-md-12 -->
+						</div><!-- /.row -->
+					</div><!-- /.col-lg-8 -->
+					<div class="col-lg-4 sidebar-column">
+						<div class="sidebar">
+							<div class="sidebar__item sidebar__item--category">
+								<h3 class="sidebar__title">CAPITAL SIRH</h3><!-- /.sidebar__title -->
+								<ul class="sidebar__category">
+									<li><a href="HRT.php">Human Ressources Tools HRT</a></li>
+									<li><a href="Capitaltime.php">Capital Time</a></li>
+									<li><a href="EspaceRH.php">Espace Ressources Humaines</a></li>
+								</ul><!-- /.sidebar__category -->
+							</div><!-- /.sidebar__item -->
+							<div class="sidebar__item sidebar__item--cta">
+								<div class="sidebar__cta"
+									style="background-image: url(../assets/images/solution/support.jpg)">
+									<i class="sidebar__cta__icon icon-phone-ringing"></i>
+									<h3 class="sidebar__cta__title">Vous avez des problèmes techniques
+									Contacter nous maintenant</h3><!-- /.sidebar__cta__title -->
+									<p class="sidebar__cta__text"><a href="tel:+212 5222-23999">+ 212 5222-23999</a>
+									<br><a href="mailto:support@capitalsoft.ma">support@capitalsoft.ma</a></p>
+									<!-- /.sidebar__cta__text -->
+								</div><!-- /.sidebar__cta -->
+							</div><!-- /.sidebar__item -->
+							<div class="sidebar__item sidebar__item--btn">
+								<a href="#" class="thm-btn sidebar__btn"><span>Téléchargez nos dépliants</span></a>
+							</div><!-- /.sidebar__item -->
+						</div><!-- /.sidebar -->
+					</div><!-- /.col-lg-4 -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section>
+
+		<div class="footer-main section-padding--bottom section-padding--top">
+			<div class="container">
+				<div class="row gutter-y-30">
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="footer-widget footer-widget--about">
+							<a href="../index.php" class="footer-widget__logo">
+								<img src="../assets/images/logo-light.png" width="119" height="40" alt="Cretech">
+							</a>
+							<p class="footer-widget__text">We work with a passion of taking challenges and creating new
+								ones in advertising sector.</p><!-- /.footer-widget__text -->
+							<ul class="footer-widget__social">
+								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+								<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+							</ul><!-- /.footer-widget__social -->
+						</div><!-- /.footer-widget -->
+					</div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="footer-widget footer-widget--links">
+							<h3 class="footer-widget__title">Services</h3><!-- /.footer-widget__title -->
+							<ul class="footer-widget__links">
+								<li>
+									<a href="service-cyber-security.html">Cyber Security</a>
+								</li>
+								<li>
+									<a href="service-it-management.html">IT Management</a>
+								</li>
+								<li>
+									<a href="service-qa-testing.html">QA & Testing</a>
+								</li>
+								<li>
+									<a href="service-infrastructure-plan.html">Infrastructure Plan</a>
+								</li>
+								<li>
+									<a href="service-it-consultent.html">IT Consultent</a>
+								</li>
+							</ul><!-- /.footer-widget__links -->
+						</div><!-- /.footer-widget -->
+					</div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="footer-widget footer-widget--newsletter">
+							<h3 class="footer-widget__title">Subscribe</h3><!-- /.footer-widget__title -->
+							<p class="footer-widget__text">Sign up to receive the latest articles</p>
+							<!-- /.footer-widget__text -->
+							<form action="#" class="footer-widget__newsletter mc-form"
+								data-url="ADD_YOUR_MAILCHIMP_FORM_URL_HERE">
+								<input type="email" name="EMAIL" placeholder="Enter Your Email">
+								<button class="thm-btn footer-widget__newsletter__btn"><span>Register</span></button>
+							</form>
+							<div class="mc-form__response"></div><!-- /.mc-form__response -->
+						</div><!-- /.footer-widget -->
+					</div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+					<div class="col-lg-3 col-md-6 col-sm-12">
+						<div class="footer-widget footer-widget--contact">
+							<h3 class="footer-widget__title">Contact</h3><!-- /.footer-widget__title -->
+							<ul class="footer-widget__contact">
+								<li>
+									<i class="fa fa-phone"></i>
+									<a href="tel:+8898006802">+ 88 ( 9800 ) 6802</a>
+								</li>
+								<li>
+									<i class="fa fa-envelope"></i>
+									<a href="mailto:needhelp@company.com">needhelp@company.com</a>
+								</li>
+								<li>
+									<i class="fa fa-map-marker-alt"></i>
+									88 Broklyn Golden Road Street <br> New York. USA
+								</li>
+							</ul><!-- /.footer-widget__contact -->
+						</div><!-- /.footer-widget -->
+					</div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</div><!-- /.footer-main -->
+		<div class="footer-bottom">
+			<div class="container">
+				<p>&copy; All Copyright <span class="dynamic-year"></span> by Cretech</p>
+			</div><!-- /.container -->
+		</div><!-- /.footer-bottom -->
+	</div><!-- /.page-wrapper -->
+
+
+	<div class="mobile-nav__wrapper">
+		<div class="mobile-nav__overlay mobile-nav__toggler"></div>
+		<!-- /.mobile-nav__overlay -->
+		<div class="mobile-nav__content">
+			<a href="#" class="mobile-nav__close mobile-nav__toggler">
+				<span></span>
+				<span></span>
+			</a>
+
+			<div class="logo-box">
+				<a href="../index.php" aria-label="logo image"><img src="../assets/images/logo-light.png" width="98"
+						height="33" alt="Cretech"></a>
+			</div>
+			<!-- /.logo-box -->
+			<div class="mobile-nav__container"></div>
+			<!-- /.mobile-nav__container -->
+
+			<ul class="mobile-nav__contact list-unstyled">
+				<li>
+					<i class="fa fa-phone"></i>
+					<a href="tel:+8898006802">+ 88 ( 9800 ) 6802</a>
+				</li>
+				<li>
+					<i class="fa fa-envelope"></i>
+					<a href="mailto:needhelp@company.com">needhelp@company.com</a>
+				</li>
+				<li>
+					<i class="fa fa-map-marker-alt"></i>
+					88 Broklyn Golden Road Street <br> New York. USA
+				</li>
+			</ul><!-- /.mobile-nav__contact -->
+			<ul class="mobile-nav__social">
+				<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+				<li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+				<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+			</ul><!-- /.mobile-nav__social -->
+
+
+
+		</div>
+		<!-- /.mobile-nav__content -->
+	</div>
+	<!-- /.mobile-nav__wrapper -->
+
+	<div class="search-popup">
+		<div class="search-popup__overlay search-toggler"></div>
+		<!-- /.search-popup__overlay -->
+		<div class="search-popup__content">
+			<form action="#">
+				<label for="search" class="sr-only">search here</label><!-- /.sr-only -->
+				<input type="text" id="search" placeholder="Search Here..." />
+				<button type="submit" aria-label="search submit" class="thm-btn">
+					<span><i class="icon-magnifying-glass"></i></span>
+				</button>
+			</form>
+		</div>
+		<!-- /.search-popup__content -->
+	</div>
+	<!-- /.search-popup -->
+
+	<a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+
+	<!-- plugins js -->
+	<script src="../assets/vendors/jquery/jquery-3.6.0.min.js"></script>
+	<script src="../assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script src="../assets/vendors/jquery-validation/jquery.validate.min.js"></script>
+	<script src="../assets/vendors/owl-carousel/owl.carousel.min.js"></script>
+	<script src="../assets/vendors/wow/wow.js"></script>
+	<script src="../assets/vendors/jarallax/jarallax.min.js"></script>
+	<script src="../assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js"></script>
+	<!-- template js -->
+	<script src="../assets/js/cretech.js"></script>
+
+</body>
+
+</html>
